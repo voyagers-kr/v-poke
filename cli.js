53fc833f5cd626cafd5c0c54f45324c1984c2c62
박수환 <vividswan@naver.com>
2023-11-25 05:46:48 +0900

    feat. 백엔드 반영

--- cli.js
+++ cli.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
-import {getPokeImageByNumber, getProjectVersion} from './src/index.js';
+import {getPokeImageByNumber, getProjectVersion, initBackend} from './src/index.js';
 import { program } from 'commander';
 
 program
     .option('-v, --version', 'version')
     .option('-gpi, --getPokeImage <value>', 'get poke image')
+    .option('-s, --start', 'start CLI Pokédex')
     .action((options) => {
         if(options.version) {
             console.log(`v-poke version ${getProjectVersion()}`);
         } else if(options.getPokeImage) {
             getPokeImageByNumber(options.getPokeImage);
+        } else if(options.start) {
+            initBackend();
         } else {
             console.log("Please enter the correct command.");
         }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { readFileSync } from 'fs';
-import { join, dirname } from 'path';
+import {readFileSync} from 'fs';
+import {dirname, join} from 'path';
 import asciiChanger from 'asciify-image';
 import fetch from 'node-fetch';
+import {spawn} from 'child_process';
+
+let isQuizProcess = false;
+let randomInt = -1;
+let nowScore = 0;
 
 // 기존의 fetch를 따로 저장
 const originalFetch = fetch;
@@ -14,6 +19,11 @@ global.fetch = (url, options) => {
 export const getPokeImageByNumber = (number) => {
     const imageRequestUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${number}.png`
     getAscii(imageRequestUrl);
+}
+
+export const getHiddenPokeImageByNumber = (number) => {
+    const imageRequestUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${number}.png`
+    getHiddenAscii(imageRequestUrl);
 }
 
 
@@ -42,3 +52,120 @@ export const getAscii = (imageUrl) => {
         console.log(asciified);
     });
 };
+
+export const getHiddenAscii = (imageUrl) => {
+
+    const options = {
+        fit:    'box',
+        width:  40,
+        height: 40
+    }
+
+    asciiChanger(imageUrl, options, function (err, asciified) {
+        if (err) throw err;
+
+        // Print to console
+        console.log(asciified.replace(/[a-zA-Z]/g, '^'));
+    });
+};
+
+export function initBackend() {
+    const javaProcess = spawn('java', ['-jar', './src/pokemon.jar']);
+    console.log('init CLI Pokédex');
+
+    // 쉘에서 문자열 입력을 읽기 위한 이벤트 핸들러
+    process.stdin.on('data', async (data) => {
+        const input = data.toString().trim();
+        const inputArray = input.split(' ');
+        if(isQuizProcess) {
+            if (isNaN(parseInt(input, 10))) {
+                console.log('Please enter Pokemon number');
+            } else {
+                if (parseInt(input, 10) === randomInt) {
+                    console.log('great!');
+                    nowScore += randomInt;
+                    console.log(`your score is ${nowScore}`);
+                    randomInt = getRandomNumber();
+                    getHiddenPokeImageByNumber(randomInt)
+                    setTimeout(()=> {
+                        console.log('please commend pokemon num')
+                    }, 2000);
+                } else {
+                    console.log('fail....');
+                    console.log(`your score is ${nowScore}`);
+                    console.log('quiz end');
+                    isQuizProcess = false;
+                    nowScore = 0;
+                }
+            }
+        } else if (input === 'exit') {
+            javaProcess.kill();
+            console.log('Backend process exited');
+            process.exit();  // 자바 프로세스가 종료되면 Node.js도 종료
+        } else if (inputArray[0] === 'info') {
+            if (inputArray.length <= 1) {
+                console.log('Please enter Pokemon number or name');
+            } else {
+                const target = inputArray[1];
+                if (!isNaN(parseInt(target, 10))) {
+                    await sendDataAndReceiveOutput(javaProcess, input).then((output) => {
+                        setTimeout(() => {
+                            console.log(output);
+                        }, 2000);
+                        getPokeImageByNumber(target);
+                    });
+                } else {
+                    await sendDataAndReceiveOutput(javaProcess, `name ${target}`).then((result) => {
+                        const resultArray = result.split(' ');
+                        if (!isNaN(parseInt(resultArray[0], 10))) {
+                            const targetIndex = parseInt(resultArray[0], 10);
+                            sendDataAndReceiveOutput(javaProcess, `info ${targetIndex}`).then((output) => {
+                                setTimeout(() => {
+                                    console.log(output);
+                                }, 2000);
+                                const outputArray = output.split(' ');
+                                getPokeImageByNumber(targetIndex);
+                            });
+                        } else {
+                            console.log('Please enter Pokemon number or name');
+                        }
+                    });
+                }
+            }
+        } else if(inputArray[0] === 'quiz') {
+            isQuizProcess = true;
+            randomInt = getRandomNumber();
+            getHiddenPokeImageByNumber(randomInt);
+            setTimeout(()=> {
+                console.log('please commend pokemon num')
+            }, 2000);
+        } else {
+            console.log("Please enter the correct command or exit!");
+        }
+    });
+
+// 자바 프로세스 종료 시 이벤트 핸들러
+    javaProcess.on('close', (code) => {
+        console.log('Backend process exited');
+        process.exit();  // 자바 프로세스가 종료되면 Node.js도 종료
+    });
+}
+
+function sendDataAndReceiveOutput(javaProcess, inputData) {
+    return new Promise((resolve) => {
+        javaProcess.stdout.once('data', (data) => {
+            const output = data.toString('utf-8');
+            resolve(output);
+        });
+
+        javaProcess.stdin.write(inputData + '\n');
+    });
+}
+
+function getRandomNumber() {
+    // 현재 시간을 이용하여 시드값 생성
+    const seed = new Date().getTime();
+
+    // 시드값을 기반으로 1부터 1017 사이의 랜덤한 정수 생성
+    return Math.floor(Math.random() * 1017) + 1;
+}
